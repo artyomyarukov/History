7743c1764199fc29d6c273c22ee80169b6248cc0
Ivan Milko <ivan.milko@yandex.ru>
2025-05-29 09:21:45 +0300

    Add small changes

--- models.py
+++ models.py
@@ -25,7 +25,7 @@ ROUTES = {
         ]
     },
     "gostinka": {
-        "name": "🌉 Гостиный двор - Невский",
+        "name": "🌉 Гостиный двор - Невский проспект",
         "link": "https://yandex.ru/maps/?um=constructor%3Ab5ad4733a6a24ea66b074a257108c613b6df135e0a4a3d4c79a6bf6ceaedafcb&source=constructorLink",
         "audios": [
             str(BASE_DIR / "assets" / "route_3" / f"{i}-gostinka-nevskiy.wav")
@@ -34,7 +34,7 @@ ROUTES = {
     },
 
     "vladimirskaya": {
-        "name": "🌉 Владимирская - Чернышевская",
+        "name": "🌃 Владимирская - Чернышевская",
         "link": "https://yandex.ru/maps/2/saint-petersburg/?ll=30.366677%2C59.922064&mode=usermaps&source=constructorLink&um=constructor%3A62e7906ac9a1362c030da8f154a8b6f5a295bb1136d92f71a6c95575a465e16b&z=12.64",
         "audios": [
             str(BASE_DIR / "assets" / "route_4" / f"{i}_vladimirskaya-chernyshevskaya.wav")
@@ -42,8 +42,17 @@ ROUTES = {
         ]
     },
 
+    "vosstaniya": {
+        "name": "🌆 Площадь Восстания - Чернышевская",
+        "link": "https://yandex.ru/maps/2/saint-petersburg/?ll=30.370519%2C59.934041&mode=usermaps&source=constructorLink&um=constructor%3Aeeca6e77b314849b8a158575a6228ed8030eb046147b1276ef5a0786184dbd30&z=14.27",
+        "audios": [
+            str(BASE_DIR / "assets" / "route_7" / f"{i}-vosstaniya-chernyshevskaya.wav")
+            for i in range(1, 7)  # 6 файлов
+        ]
+    },
+
     "chernaya-rechka": {
-        "name": "🌉 Черная речка - Старая Деревня",
+        "name": "🎑 Черная речка - Старая Деревня",
         "link": "https://yandex.ru/maps/?um=constructor%3A5385f8bde711a9f3a3ffc9d5d729b55abfb0cf5ff7032e15af01080368e6ea2c&source=constructorLink",
         "audios": [
             str(BASE_DIR / "assets" / "route_5" / f"{i}-chernaya-rechka-staraya-derevnya.wav")
@@ -52,20 +61,11 @@ ROUTES = {
     },
 
     "krestovskij": {
-        "name": "🌉 Крестовский остров - Беговая",
+        "name": "🎢🎪 Крестовский остров - Беговая",
         "link": "https://yandex.ru/maps/?um=constructor%3A11c9db94d539fa3d2c5fc8cded29afcf8a1451358eb70c9e4d80e024d3d2751a&source=constructorLink",
         "audios": [
             str(BASE_DIR / "assets" / "route_6" / f"{i}-krestovskij-begovaya.wav")
             for i in range(1, 3)  # 2 файлов
-        ]
-    },
-
-    "vosstaniya": {
-        "name": "🌉 Площадь Восстания - Чернышевская",
-        "link": "https://yandex.ru/maps/2/saint-petersburg/?ll=30.370519%2C59.934041&mode=usermaps&source=constructorLink&um=constructor%3Aeeca6e77b314849b8a158575a6228ed8030eb046147b1276ef5a0786184dbd30&z=14.27",
-        "audios": [
-            str(BASE_DIR / "assets" / "route_7" / f"{i}-vosstaniya-chernyshevskaya.wav")
-            for i in range(1, 7)  # 6 файлов
         ]
     },
 }
